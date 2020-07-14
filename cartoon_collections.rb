8aaa49b9677e931398f6b754c72d73ea9e15a2f9
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-14 05:58:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array_of_dwarf_names)
  end
 
 
-def summon_captain_planet(planeteer_)
+def summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
