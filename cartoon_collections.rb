8178e64292a71704a83fde5fa79189989dd5d021
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-14 06:49:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 
 def find_the_cheese(cheesy_items)
-  cheeses =[gouda cheddar camembert]
+  cheeses =["gouda", "cheddar", "camembert"]
   cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
