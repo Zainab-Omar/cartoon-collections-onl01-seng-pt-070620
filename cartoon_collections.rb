79eaddd5c748eb4751325bcd5762e636e53f996c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-14 06:07:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ def roll_call_dwarves(array_of_dwarf_names)
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |planeteer|
-    #up_case= 
-    planeteer.upcase
+    up_case= planeteer.upcase
   #"#{up_case}!"
   end
 end
