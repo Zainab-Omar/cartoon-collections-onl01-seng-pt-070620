c587eff023f37e857d7aefe584e640dd62a8cf3e
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-14 05:57:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,8 @@
 def roll_call_dwarves(array_of_dwarf_names)
-  #new_array=[]
   array_of_dwarf_names.each_with_index do |name, index|
   puts "#{index+1} #{name}"
   end
-  #new_array
-end
+ end
 
 
 def summon_captain_planet# code an argument here
